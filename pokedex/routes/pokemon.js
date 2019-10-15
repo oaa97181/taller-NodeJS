afe14ac28be3053ecf615c256447a242fa3b60d4
oaa97181 <oaa97181@hotmail.com>
2019-10-15 17:23:26 -0500

    UPDATE with QueryBuilder

--- pokedex/routes/pokemon.js
+++ pokedex/routes/pokemon.js
@@ -1,7 +1,7 @@
 const pokedex = require('../pokedex.json');
 const db= require('../config/database.js');
 const express = require('express');
-const pokerouter = express.Router(); // PRENDER XAMPP ANTES DE INICIAR JEJE
+const pokerouter = express.Router(); // PRENDER XAMPP ANTES DE INICIAR JEJE // APACHE Y MYSQL
 
 pokerouter.get("/", (req, res) => {
 	// res.status(201).json(pokedex)	
@@ -29,9 +29,37 @@ pokerouter.post("/", (req, res) => {
 		res.status(500);
 		res.send("algo falló mientras se agregaba a la BD");
 	});
-
-
 });
+
+pokerouter.delete("/:id([0-9]{1,3})",(req,res) => {
+	query=`DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+	db.query(query).then(rows =>{
+		res.status(200);
+		console.log(rows);
+		res.send("Pokemon eliminado correctamente");
+	}).catch(err=>{
+		console.log(err);
+		res.status(500);
+		res.send("Ocurrió algo mal");
+	});
+});
+
+
+pokerouter.put("/:id([0-9]{1,3})", (req,res)=> {
+	const columns= Object.keys(req.body)
+	const values= Object.values(req.body)
+	query= "UPDATE pokemon SET ";
+	for (let i = 0; i<columns.length; i++) {
+		// condicion ? verdadero : falso
+		query+=  `${columns[i]} =`
+		query+= isNaN(values[i]) ? `'${values[i]}'` : `${values[i]}`
+		query+= (i+1<columns.length) ? ", " : " "
+	}
+	query+=` WHERE pok_id = ${req.params.id}`
+	res.send(query)
+})
+
+
 
 pokerouter.get('/random', (req, res) => {
   const randpoke=Math.floor((Math.random() * 722) + 1);
@@ -48,7 +76,7 @@ pokerouter.get('/random', (req, res) => {
 
 
 
-pokerouter.get('/:id([0-9])', (req, res) => {
+pokerouter.get('/:id([0-9]{1,3})', (req, res) => {
 	const id = req.params.id;
 	const query= `SELECT * FROM pokemon WHERE pok_id='${id}'`;
 		db.query(query).then(rows=> {

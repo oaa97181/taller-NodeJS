f48ce332504a416c9f11d6ad1be32bd138718aa4
oaa97181 <oaa97181@hotmail.com>
2019-10-22 17:25:45 -0500

    made an htdocs folder to consume the API

--- pokedex/index.js
+++ pokedex/index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const pokemon= require('./routes/pokemon.js');
 const moves= require('./routes/moves.js');
 const notFoundHandler= require('./middleware/notFoundHandler.js')
+const corsHandler = require('./middleware/corsHandler.js')
 const morgan=require('morgan');
 var app = express();
 
+app.use(corsHandler)
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
